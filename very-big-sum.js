b76d97207bb51805a7b25c68ae60ba9a200d0fa4
Saundie Weiss <saundieweiss@gmail.com>
2016-05-11 09:50:43 -0600

    Added very big sum

---
+++ very-big-sum.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// You are given an array of integers of size NN. You need to print the sum of the elements in the array,
+//keeping in mind that some of those integers may be quite large.
+//
+// Input
+// The first line of the input consists of an integer NN. The next line contains NN space-separated integers contained in the array.
+//
+// Constraints
+// 1≤N≤101≤N≤10
+// 0≤A[i]≤10100≤A[i]≤1010
+//
+// Sample Input
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+//
+// Output
+// Print a single value equal to the sum of the elements in the array. In the above sample, you would print 5000000015
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+
+}
