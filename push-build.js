7d5146c33f1fec6cfa1d1f4b7217fee6197550f6
Saundie Weiss <saundieweiss@gmail.com>
2016-05-16 10:03:42 -0600

    Added push-build exercise from CodeWars

---
+++ push-build.js
@@ -0,0 +1,20 @@
+'use strict';
+// Write push() and buildOneTwoThree() functions to easily update and initialize linked lists.
+//Try to use the push() function within your buildOneTwoThree() function.
+
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function push(head, data) {
+  // Go.
+  return 'push';
+}
+
+function buildOneTwoThree() {
+  // Go.
+}
+
+console.log(push());
--- very-big-sum.js
+++ very-big-sum.js
@@ -16,10 +16,37 @@
 //
 // Output
 // Print a single value equal to the sum of the elements in the array. In the above sample, you would print 5000000015
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function(data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function() {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
 
 function main() {
     var n = parseInt(readLine());
     arr = readLine().split(' ');
     arr = arr.map(Number);
+    var sum = 0;
+    for (var i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
 
 }
+
+
+// main(5, [1000000001 1000000002 1000000003 1000000004 1000000005]);
