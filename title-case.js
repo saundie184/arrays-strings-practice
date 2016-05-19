6a2cd3cdcaad481adb4d421924e28f41d42943ea
Saundie Weiss <saundieweiss@gmail.com>
2016-05-19 10:44:31 -0600

    Added title case challenge

---
+++ title-case.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function titleCase(title, minorWords) {
+//loop throught title
+//push all the words to an array that are not in minorWords
+//always push the first word to the array
+
+//uppercase each first letter of each item in the array
+
+return title;
+}
+
+
+console.log(titleCase('a clash of KINGS', 'a an the of')); // should return: 'A Clash of Kings'
+// console.log(titleCase('THE WIND IN THE WILLOWS', 'The In')); // should return: 'The Wind in the Willows'
+// console.log(titleCase('the quick brown fox')); // should return: 'The Quick Brown Fox'
